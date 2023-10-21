e05b8893ab53d26c3d236f59709406d04e8f714e
Renee Perpignan <reneeperpignan@dhcp-10-250-58-204.harvard.edu>
2023-10-21 15:41:08 -0400

    charts

---
+++ chart/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import LineChart from "./linechart.js"
+import FilledLineChart from "./filledlinechart.js"
+
+export default function Home() {
+  return (
+    <div>
+
+
+    <LineChart/>
+    {/* <FilledLineChart/> */}
+    </div>
+  )
+}
