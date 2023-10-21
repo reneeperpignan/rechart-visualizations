e4a84f7a132dc1eac0b29aa44f6e9e5498a78133
Fikir Teklemedhin <fi.leul3562@gmail.com>
2023-10-21 18:10:45 -0400

    line visualizations using rechart and react-chartjs-2

--- chart/app/page.tsx
+++ chart/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from 'next/image'
-import LineChart from "./linechart.js"
+
 import FilledLineChart from "./filledlinechart.js"
+import RechartsLine from './RechartsLine.jsx'
+import ChartsLine from './ChartsLine.jsx'
+import ChartsBar from "./ChartsBar.jsx"
 
 export default function Home() {
   return (
     <>
     {/* <LineChart/> */}
     <FilledLineChart/>
+    {RechartsLine()} 
+    {ChartsBar()} 
+    {ChartsLine()}
     </>
   )
 }
